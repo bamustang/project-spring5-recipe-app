b91c97ac91db1ee614ea74b80b14427d26376e28
Bryan <bpaaron@gmail.com>
2020-04-23 18:19:17 -0400

    last code clean up

--- src/main/java/guru/springframework/recipes/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipes/controllers/IndexController.java
@@ -1,15 +1,10 @@
 package guru.springframework.recipes.controllers;
-import guru.springframework.recipes.model.Category;
 import guru.springframework.recipes.repository.CategoryRepository;
 import guru.springframework.recipes.repository.RecipeRepository;
 import guru.springframework.recipes.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 @Controller
 public class IndexController {
@@ -26,17 +21,8 @@ public class IndexController {
 
     @RequestMapping({"", "/","/index","/index.htm", "/index.html"})
     public String showIndexPage (Model model) {
-        System.out.println(">>showIndexPage Handler for /, /index, /index.htm, /index.html");
-        System.out.println("ID for tablespoon is "+uomRepository.findByUnitOfMeasure("tablespoon").get().getId());
-        System.out.println("ID for Tex-Mex is "+categoryRepository.findByCategory("Tex-Mex").get().getId());
-        System.out.println("Recipe count = "+recipeRepository.count());
-
         model.addAttribute("recipes", recipeRepository.findAll());
-        List<String> foodCategories = new ArrayList<String>();
-        Iterable catSet = categoryRepository.findAll();
-        //catSet.forEach(category -> foodCategories.add());
-        model.addAttribute("categories", catSet);
-
+        model.addAttribute("categories", categoryRepository.findAll());
         return("index");
 
 
